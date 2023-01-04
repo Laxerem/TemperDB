8909d00ae1476639be1c2ca9e38a325c8f20a977
Grisha Vorobyev <grishamas@yandex.ru>
2023-01-04 17:18:50 +0500

    UPDATE project

--- main2.py
+++ main2.py
@@ -7,12 +7,13 @@ db.query("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY AUTOINCREMENT,
 
 t1 = TempWriter()
 t1.start()
+t1.join()
 
-print("Введите дату и время")
-print("К примеру: 02.01.2023, 22:40")
-
-date2 = str(input())
-time2 = str(input())
-
-r = db.fetchall(f"SELECT value FROM data WHERE date = '{date2}' and time = '{time2}'")
-print(r)
+# print("Введите дату и время")
+# print("К примеру: 02.01.2023, 22:40")
+#
+# date2 = str(input())
+# time2 = str(input())
+#
+# r = db.fetchall(f"SELECT value FROM data WHERE date = '{date2}' and time = '{time2}'")
+# print(r)
--- thread.py
+++ thread.py
@@ -14,12 +14,12 @@ def read_temp():
 class TempWriter(Thread):
 
     def __init__(self):
-        super().__init__()
+        super().__init__(daemon=True)
         self.db = DataBase()
 
     def run(self):
         while True:
-            value = read_temp()
+            value = read_temp() / 1000
             date = datetime.datetime.now()
             self.db.query(f"INSERT INTO data (date, time, value) VALUES ('{date.strftime('%d.%m.%Y')}', '{date.strftime('%H:%M')}', {value})")
             time.sleep(60)
